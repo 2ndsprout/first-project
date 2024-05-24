305ee3eaa912a3932a9879b05cd5299ca843205e
2ndsprout <ralphlauren5243@gmail.com>
2024-05-24 15:48:49 +0900

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/main/resources/templates/**/*.{html,js}",
-    "./src/**/*.{html,js}"
+    "./src/main/resources/templates/**/*.{html,js,css}",
+    "./src/**/*.{html,js,css}"
   ],
   theme: {
     extend: {},
