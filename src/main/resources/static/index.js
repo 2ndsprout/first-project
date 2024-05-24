5fb03779246493cae95adba361bb028b39540ea6
2ndsprout <ralphlauren5243@gmail.com>
2024-05-24 17:07:59 +0900

    add to navbar and tailwind toast editor update

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -5,17 +5,19 @@ require('@toast-ui/editor/dist/toastui-editor-viewer.css');
 require('@toast-ui/editor/dist/theme/toastui-editor-dark.css');
 require('prismjs/themes/prism.css');
 require('@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css');
+require('tui-color-picker/dist/tui-color-picker.css');
+require('@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css');
 
-const codeSyntaxHighlight = require('@toast-ui/editor-plugin-code-syntax-highlight');
-
-
+const codeSyntaxHighlight = require('@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js');
+const codeSyntax = require('@toast-ui/editor-plugin-color-syntax');
+const Prism = require('prismjs');
 
 const editor = new Editor({
   el: document.querySelector('#editor'),
-  height: '600px',
-  width: '500px',
+  height: 'auto',
+  width: 'auto',
   initialEditType: 'markdown',
   previewStyle: 'vertical',
   theme: 'dark',
-  plugins: [codeSyntaxHighlight]
+  plugins: [codeSyntax, codeSyntaxHighlight]
 });
