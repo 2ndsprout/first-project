03e00f72bf88ccc932928e8cece4bc84a5371d83
2ndsprout <ralphlauren5243@gmail.com>
2024-05-28 16:15:38 +0900

    basic setting

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -14,8 +14,7 @@ const Prism = require('prismjs');
 
 const editor = new Editor({
   el: document.querySelector('#editor'),
-  height: '700px',
-  width: '700px',
+  height: '800px',
   initialEditType: 'wysiwyg',
   previewStyle: 'vertical',
   theme: 'dark',
