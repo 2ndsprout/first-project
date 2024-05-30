0f935fbb3ff11f442ceb641e91b6d49f8c882e28
2ndsprout <ralphlauren5243@gmail.com>
2024-05-30 16:44:55 +0900

    profile form add

--- tail_build.bat
+++ tail_build.bat
@@ -1,1 +1,7 @@
 npx tailwindcss -i ./src/main/resources/static/input.css -o ./src/main/resources/static/dist/output.css --watch
+
+npm uninstall tailwindcss postcss autoprefixer
+npm install -D tailwindcss postcss autoprefixer
+npm cache clean --force
+npm install
+
